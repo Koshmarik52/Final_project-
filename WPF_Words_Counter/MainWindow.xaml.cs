8d6b1116cfca013c75a56b6c35eb956d4f7ae217
Koshmarik52 <skynightmare_7@mail.ru>
2025-05-31 11:21:17 +0300

    Add files via upload

---
+++ WPF_Words_Counter/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Words_Counter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 taskWindow = new Window1();
+            taskWindow.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string PassWord = Password.Text;
+            if (PassWord == "Unn123")
+            {
+                MessageBox.Show("Добро пожаловать :)");
+                MessageBox.Show("Перед началом работы с приложением очистите поле при помощи кнопки Очистить... и пожалуйста, не не выводите их листа текст, если строка пуста");
+                Window2 taskWindow = new Window2();
+                taskWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неверный пароль");
+            }
+        }
+    }
+}
---
+++ WPF_Words_Counter/Window2.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WPF_Words_Counter
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void WordsQuantity_Click(object sender, RoutedEventArgs e)
+        {
+            string inputText = VvediteText.Text;
+            int WordCount = CountWords(inputText);
+            MessageBox.Show("Количество слов в тексте: " + WordCount);
+        }
+        private int CountWords(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                {
+                    return 0;
+                }
+
+            char[] delimiters = new char[] { ' ', '\n', '\r', '\t', ',', '.', '!', '?' };
+            string[] words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+        
+        private void SymbolsFrequency_Click(object sender, RoutedEventArgs e)
+        {
+            string text = VvediteText.Text;
+            int charCount = text.Length;
+            MessageBox.Show("Количество символов: " + charCount.ToString());
+        }
+
+        private void SymbolsQuantity_Click(object sender, RoutedEventArgs e)
+        {
+            string text = VvediteText.Text;
+            string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?', ';', ':', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    wordFrequency[word]++;
+                }
+                else
+                {
+                    wordFrequency[word] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in wordFrequency.OrderByDescending(key => key.Value))
+            {
+                MessageBox.Show(pair.Key + ": " + pair.Value);
+            }
+        }
+
+        private void ClearText_Click(object sender, RoutedEventArgs e)
+        {
+            VvediteText.Text = "";
+            VvediteText.Clear();
+        }
+
+        private void TextSaving_Click_1(object sender, RoutedEventArgs e)
+        {
+            string inputText = VvediteText.Text;
+
+            if (!string.IsNullOrWhiteSpace(inputText))
+                {
+                    SaveList.Items.Add(inputText);
+                    VvediteText.Clear();
+                }
+        }
+        private void OutputSavedText_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string selectedText = SaveList.SelectedItem.ToString();
+                VvediteText.Text = selectedText;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Я же сказала не выводить! >:(");
+            }
+        }
+    }
+}
